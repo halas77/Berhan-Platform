e218c2a4a5a1d22bddf1921618a35add367f2c15
Dawit <138193176+halas77@users.noreply.github.com>
2023-10-12 15:55:42 -0400

    Commit

---
+++ app/test/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='max-h-screen w-full font-sans'>
+        <div className='flex justify-center p-10'>
+            <div className='border-l-4 border-blue-400 border rounded-xl shadow-xl w-[1000px] h-[400px] p-6 '>
+                <h2 className='pb-3 text-lg '>1. This is question number one.</h2>
+                <p>A. Choice one</p>
+                <p>B. Choice two</p>
+                <p>C. Choice three</p>
+                <p>D. Choice four</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Test
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,39 +1,14 @@
 "use client";
 
-import React from "react";
-import { motion } from "framer-motion";
-
 const Hero = () => {
   return (
-    <div className="Hero-section w-full h-screen flex items-center overflow-hidden">
-      {/* Left Column for Text */}
-      <div className="z-10 w-full p-8 text-white">
-        <motion.div
-          className="relative h-screen flex items-center overflow-hidden"
-          initial={{ opacity: 0, y: 0, x: -60 }}
-          animate={{ opacity: 1, y: 0, x: 0 }}
-          transition={{ duration: 1.2, ease: "easeOut" }}
-        >
-          <div className="mx-auto text-center">
-            <h1 className="text-6xl md:text-8xl font-semibold mb-4">
-              Hello, <br />
-              Welcome to <br />
-              <span className="text-yellow-500 md:text-8xl font-extrabold">
-                {" "}
-                Berhan
-              </span>
-            </h1>
-            <p className="text-xl pt-4 pl-2 text-yellow-50 font-light">
-              Breaking Barriers, Unleashing Potentials
-            </p>
-            <button className="mt-10 ml-2 bg-opacity-5 border border-yellow-600 font-semibold text-lg  px-8 py-3  text-white rounded-lg transition duration-300 bg-yellow-600">
-              Get Started
-            </button>
-          </div>
-        </motion.div>
+    <div className="max-h-screen w-full text-white">
+      <div className="flex items-center justify-evenly py-10">
+        <div className="max-w-3xl">
+          <h2></h2>
+        </div>
+        <div>hello</div>
       </div>
-
-      {/* Right Column for Image */}
     </div>
   );
 };
